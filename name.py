5ecf7410dc90003a2a1f4192320e092f3dce9ae5
AidenVu1 <100857445+AidenVu1@users.noreply.github.com>
2023-03-07 15:32:41 +0000

    works

--- name.py
+++ name.py
@@ -1,1 +1,7 @@
-
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+bd = input("Did you have your birthday this year?: ")
+year = 2023-age
+if bd == 'no':
+    year=year-1
+print(name + ", you are born in " + str(year))
